f3a72a35f363076db08cc3b9b9aece31287e5335
Clems GDF <clemsphoto@yahoo.fr>
2019-03-28 10:20:46 +0100

    liste heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({ /*toujours mettre un @ pour annoter la classe du component */
   selector: 'app-heroes',
@@ -7,15 +8,23 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  /* hero ='Windstorm'; */
+  /* hero ='Windstorm'; 
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  };
+  }; */
+
+  heroes = HEROES; 
+
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Olymphe' },
+  { id: 2, name: 'Morphé' },
+  { id: 3, name: 'Hermes' },
+  { id: 4, name: 'Zeus' },
+  { id: 5, name: 'Athéna' },
+  { id: 6, name: 'Poséidon' },
+  { id: 7, name: 'Apollon' },
+  { id: 8, name: 'Hades' },
+  { id: 9, name: 'Athena' },
+  { id: 10, name: 'Persé' }
+];
